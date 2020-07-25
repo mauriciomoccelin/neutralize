efea97078bcff238a919ea6b7aa627d9676a9d85
Mauricio Moccelin <mauriciomoccellin@hotmail.com>
2020-07-25 12:18:44 -0300

    Feature BuildingBlocks.Http (#1)
    * Implementation of the http base call library in HttpClient
    
    * Renamed interface IClient -> IHttpClient
    
    * Console the exception when TryDeserializeAs fails
    
    * Add tests for BuildingBlocks.Http
    
    * Add API Mock for BuildingBlocks.Http.Tests

---
+++ src/BuildingBlocks.Http/IHttpClient.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.Http
+{
+    public interface IHttpClient : IDisposable
+    {
+        #region Get
+        Task<Result<TData>> GetAsync<TData>(
+            string uri,
+            Authorization authorization = default(Authorization),
+            CancellationToken cancellationToken = default(CancellationToken)
+        );
+        #endregion
+
+        #region Post
+        Task<Result> PostAsync(
+            string uri,
+            Authorization authorization = default(Authorization),
+            CancellationToken cancellationToken = default(CancellationToken)
+        );
+        
+        Task<Result<TData>> PostAsync<TPostData, TData>(
+            string uri,
+            TPostData data,
+            Authorization authorization = default(Authorization),
+            CancellationToken cancellationToken = default(CancellationToken)
+        );
+        
+        Task<Result> PostAsync<TPostData>(
+            string uri,
+            TPostData data,
+            Authorization authorization = default(Authorization),
+            CancellationToken cancellationToken = default(CancellationToken)
+        );
+
+        Task<Result<TData>> PostAsync<TData>(
+            string uri,
+            Authorization authorization = default(Authorization),
+            CancellationToken cancellationToken = default(CancellationToken)
+        );
+        #endregion
+        
+        #region Put
+        Task<Result> PutAsync(
+            string uri,
+            Authorization authorization = default(Authorization),
+            CancellationToken cancellationToken = default(CancellationToken)
+        );
+        
+        Task<Result<TData>> PutAsync<TPutData, TData>(
+            string uri,
+            TPutData data,
+            Authorization authorization = default(Authorization),
+            CancellationToken cancellationToken = default(CancellationToken)
+        );
+        
+        Task<Result> PutAsync<TPutData>(
+            string uri,
+            TPutData data,
+            Authorization authorization = default(Authorization),
+            CancellationToken cancellationToken = default(CancellationToken)
+        );
+
+        Task<Result<TData>> PutAsync<TData>(
+            string uri,
+            Authorization authorization = default(Authorization),
+            CancellationToken cancellationToken = default(CancellationToken)
+        );
+        #endregion
+
+        #region Delete
+        Task<Result> DeleteAsync(
+            string uri,
+            Authorization authorization = default(Authorization),
+            CancellationToken cancellationToken = default(CancellationToken)
+        );
+        #endregion
+    }
+}
