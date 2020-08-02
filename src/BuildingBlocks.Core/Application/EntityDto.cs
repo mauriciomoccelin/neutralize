39e7bd04971efbd6eec233d23ca66092bdf90228
Mauricio Moccelin <mauriciomoccellin@hotmail.com>
2020-08-02 18:17:00 -0300

    Fix stack overflow in dotnet pipeline

--- src/BuildingBlocks.Core/Application/EntityDto.cs
+++ src/BuildingBlocks.Core/Application/EntityDto.cs
@@ -1,16 +1,9 @@
-using System;
-
 namespace BuildingBlocks.Core.Application
 {
     public class EntityDto<TDto, TId> : IEntityDto<TDto, TId> where TDto : class where TId : struct
     {
-        private readonly TDto empty = Activator.CreateInstance<TDto>(); 
-        
         public TId Id { get; set; }
 
         public EntityDto() { }
-        
-        public TDto Empty() => empty;
-        public bool IsEmpty(TDto dto) => empty.Equals(dto);
     }
 }
--- src/BuildingBlocks.Core/Application/IEntityDto.cs
+++ src/BuildingBlocks.Core/Application/IEntityDto.cs
@@ -2,7 +2,5 @@ namespace BuildingBlocks.Core.Application
 {
     public interface IEntityDto<TDto, TId> where TDto: class where TId: struct
     {
-        TDto Empty();
-        bool IsEmpty(TDto dto);
     }
 }
