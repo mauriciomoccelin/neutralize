0593d1069dba1fad62926cc9a00af47f9c53bc75
Mauricio Moccelin <mauriciomoccellin@hotmail.com>
2020-08-15 19:04:03 -0300

    Add ComboBoxDto

---
+++ src/BuildingBlocks.Core/Application/ComboBoxDto.cs
@@ -0,0 +1,15 @@
+namespace BuildingBlocks.Core.Application
+{
+    public class ComboBoxDto<TValue> where TValue : struct
+    {
+        public TValue Value { get; set; }
+        public string DisplayText { get; set; }
+
+        public ComboBoxDto() { }
+        public ComboBoxDto(TValue value, string displayText)
+        {
+            Value = value;
+            DisplayText = displayText;
+        }
+    }
+}
