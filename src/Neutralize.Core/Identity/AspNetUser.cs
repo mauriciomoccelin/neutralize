e5f0146a6fea01ac3373cf51d2c5a0db297ca4a4
Mauricio Moccelin <mauriciomoccellin@hotmail.com>
2022-01-27 14:26:44 -0300

    Add IAspNetUser implementation

---
+++ src/Neutralize.Core/Identity/AspNetUser.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+
+namespace Neutralize.Identity
+{
+    public class AspNetUser : IAspNetUser
+    {
+        private readonly IHttpContextAccessor accessor;
+
+        public AspNetUser(IHttpContextAccessor accessor)
+        {
+            this.accessor = accessor;
+        }
+
+        public string Name => accessor.HttpContext.User.Identity.Name;
+
+        public IEnumerable<Claim> GetUserClaims()
+        {
+            return accessor.HttpContext.User.Claims;
+        }
+
+        public Claim GetClaim(string type)
+        {
+            return GetUserClaims().First(claim => claim.Type == type);
+        }
+
+        public string GetUserEmail()
+        {
+            return IsAutenticated() ? accessor.HttpContext.User.GetUserEmail() : "";
+        }
+
+        public long GetUserId()
+        {
+            long.TryParse(accessor.HttpContext.User.GetUserId(), out var id);
+            return IsAutenticated() ? id  : default;
+        }
+
+        public bool IsAutenticated()
+        {
+            return accessor.HttpContext.User.Identity.IsAuthenticated;
+        }
+
+        public HttpContext GetHttpContext()
+        {
+            return accessor.HttpContext;
+        }
+    }
+}
---
+++ src/Neutralize.Core/Identity/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Neutralize.Identity
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+            {
+                throw new ArgumentException(null, nameof(principal));
+            }
+
+            var claim = principal.Claims.Single(c => c.Type == ClaimTypes.NameIdentifier);
+            return claim?.Value;
+        }
+
+        public static string GetUserEmail(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+            {
+                throw new ArgumentException(null, nameof(principal));
+            }
+
+            var claim = principal.Claims.Single(c => c.Type == ClaimTypes.Email);
+            return claim?.Value;
+        }
+    }
+}
