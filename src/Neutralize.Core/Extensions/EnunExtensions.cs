a0af0548ec97e7bba8a1dd7624e7126bf4198325
Mauricio Moccelin <mauriciomoccellin@hotmail.com>
2022-02-01 07:31:22 -0300

    Enum extensions (#18)
    * Add enum extensions for get enum description
    
    * Add enum extensions tests
    
    * Update configuration.json

---
+++ src/Neutralize.Core/Extensions/EnunExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Neutralize.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum value, bool upper = true)
+        {
+            var fi = value.GetType().GetField(value.ToString());
+            var attributes = fi.GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[];
+
+            var description = string.Empty;
+            if (attributes != null && attributes.Any())
+            {
+                description = attributes.First().Description;
+            }
+
+            if (upper) description = description.ToUpper();
+
+            return description;
+        }
+    }
+}
