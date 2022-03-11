9cc7ad040aedb2bca5a5c355383058020a970e42
Mauricio Moccelin <mauriciomoccellin@hotmail.com>
2022-03-11 05:19:34 -0300

    Add option to ignore email sending

--- src/Neutralize.SendGrid/SendGrid/SendGridEmailSenderOption.cs
+++ src/Neutralize.SendGrid/SendGrid/SendGridEmailSenderOption.cs
@@ -5,6 +5,7 @@ namespace Neutralize.SendGrid
     public class SendGridEmailSenderOption : ISendGridEmailSenderOption
     {
         public string ApiKey { get; set; }
+        public bool IgnoreEmailSending { get; set; }
         public EmailAddress EmailAddressFrom { get; set; }
 
         public string GetApiKey() => ApiKey;
@@ -18,6 +19,13 @@ namespace Neutralize.SendGrid
         public ISendGridEmailSenderOption SetEmailFrom(string email, string name)
         {
             EmailAddressFrom = new EmailAddress(email, name);
+            return this;
+        }
+
+        public bool GetIgnoreEmailSending() => IgnoreEmailSending;
+        public ISendGridEmailSenderOption SetIgnoreEmailSending(bool value)
+        {
+            IgnoreEmailSending = value;
             return this;
         }
     }
